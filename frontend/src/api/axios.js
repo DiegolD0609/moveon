2642d6bf211befb812d04fbea558dfb3bf9ec202
Mortassesin0609@ <diego.lugo.06@outlook.es>
2025-04-14 02:03:54 -0600

    update axios.1

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: VITE_API_BASE_URL, // Directly using your backend URL
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Directly using your backend URL
   headers: {
     'Content-Type': 'application/json'
   }
